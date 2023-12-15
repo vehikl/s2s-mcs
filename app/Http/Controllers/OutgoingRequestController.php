322d564ada0b71d902bbfd9aa64a11c53c4558fb
ryanennns <ryanennns@gmail.com>
2023-12-15 14:03:55 -0500

    OutgoingRequest setup

---
+++ app/Http/Controllers/OutgoingRequestController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OutgoingRequest;
+use Illuminate\Http\Request;
+
+class OutgoingRequestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(OutgoingRequest $outgoingRequest)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(OutgoingRequest $outgoingRequest)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, OutgoingRequest $outgoingRequest)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(OutgoingRequest $outgoingRequest)
+    {
+        //
+    }
+}
