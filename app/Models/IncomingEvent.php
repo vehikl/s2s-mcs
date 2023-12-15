b440088a9cf22ac4f8480ce707c21d0f164be1e2
ryanennns <ryanennns@gmail.com>
2023-12-15 14:11:35 -0500

    setup relationships between IncomingEvent and OutgoingRequest

--- app/Models/IncomingEvent.php
+++ app/Models/IncomingEvent.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class IncomingEvent extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    public function outgoingRequests(): HasMany
+    {
+        return $this->hasMany(OutgoingRequest::class, 'event_id');
+    }
 }
--- app/Models/OutgoingRequest.php
+++ app/Models/OutgoingRequest.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class OutgoingRequest extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function incomingEvent(): BelongsTo
+    {
+        return $this->belongsTo(IncomingEvent::class, 'event_id');
+    }
 }
